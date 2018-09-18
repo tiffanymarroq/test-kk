c2127238b995eb951890990816666d2281bc1732
Tiffany Marroquin <tiffany.marroq@gmail.com>
2018-09-18 19:06:40 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1>Workers</h1>
+    <input type="text">Search for
+    <div v-for="i in employees">
+      <h1>{{i.title}}</h1>
+      <p>{{i.name}}</p>
+      <p>{{i.email}}</p>
+      <p>{{i.office}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'app',
+  data () {
+    return{
+      employees: []
+      
+    }
+  },
+  created(){
+    axios.get('/src/assets/FakeOrgJSON.json')
+    .then(data => {
+      this.employees = data.data
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
